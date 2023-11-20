0fe0335bad3bc1f839ac4c6f7d8f70cbc949c2f4
Emanuel <emanuelmarcolongo@gmail.com>
2023-11-20 18:02:30 -0300

    chore: create products services

--- src/app/services/products/products.service.ts
+++ src/app/services/products/products.service.ts
@@ -1,9 +1,26 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class ProductsService {
+  constructor(private http: HttpClient) {}
 
-  constructor() { }
+  getProducts(): Observable<any> {
+    return this.http.get('http://localhost:3000/products');
+  }
+
+  insertProduct(body: any): Observable<any> {
+    return this.http.post('http://localhost:3000/products', body);
+  }
+
+  deleteProduct(id: number): Observable<any> {
+    return this.http.delete(`http://localhost:3000/products/${id}`);
+  }
+
+  editProduct(id: number, body: any): Observable<any> {
+    return this.http.put(`http://localhost:3000/products/${id}`, body);
+  }
 }
