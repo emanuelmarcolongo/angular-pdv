5220f10695745541514d590c1b46d155ddbd4d21
Emanuel <emanuelmarcolongo@gmail.com>
2023-11-21 19:36:22 -0300

    feat: product list route

--- src/app/components/products/list-products/list-products.component.ts
+++ src/app/components/products/list-products/list-products.component.ts
@@ -1,10 +1,24 @@
 import { Component } from '@angular/core';
+import { ProductsService } from 'src/app/services/products/products.service';
 
 @Component({
   selector: 'app-list-products',
   templateUrl: './list-products.component.html',
-  styleUrls: ['./list-products.component.scss']
+  styleUrls: ['./list-products.component.scss'],
 })
 export class ListProductsComponent {
+  products!: any;
+  user!: any;
+  isAdmin!: boolean;
 
+  constructor(public productService: ProductsService) {
+    this.productService.getProducts().subscribe((products: any[]) => {
+      this.products = products;
+    });
+
+    const user = localStorage.getItem('User');
+    if (user) {
+      this.user = JSON.parse(user);
+    }
+  }
 }
